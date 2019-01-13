c1f64d6e8ce26cac5666d7d3d794a32db37877d3
cici cemong <45526554+cicicemong@users.noreply.github.com>
2019-01-13 11:58:40 +0700

    Add files via upload

---
+++ MENGHITUNG UPAH PERJAM.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+
+{
+    int j,hasil;
+    cout<<"Program menghitung upah perjam\n";
+    cout<<"================================";
+    cout<<"Masukan jumlah jam             =";cin>>j;
+    hasil=j*5000;
+    cout<<"Jumlah upah keseluruhan        ="<<hasil;
+    return 0;
+
+}
